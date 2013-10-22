c934394dfabfc346d6eb122a33b6c9737a3706ae
Nicolai Christensen <nicolai_christensen@hotmail.com>
2013-10-22 15:35:17 +0200

    Created plane

---
+++ Findalls Tower/Assets/TiltPlane.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TiltPlane : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetButton("Fire1") && this.transform.rotation.eulerAngles.x < 45) 
+		{
+			this.transform.Rotate(new Vector3(1,0,0), 5);
+		}
+	}
+}
