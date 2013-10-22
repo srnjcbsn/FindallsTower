d7dcfa2d5e65c0cc3a3f8074e08b3397b87aca0f
Nicolai Christensen <nicolai_christensen@hotmail.com>
2013-10-22 16:46:05 +0200

    Experimented with the plane

--- Findalls Tower/Assets/TiltPlane.cs
+++ Findalls Tower/Assets/TiltPlane.cs
@@ -13,9 +13,43 @@ public class TiltPlane : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetButton("Fire1") && this.transform.rotation.eulerAngles.x < 45) 
+		if (Input.GetAxis("Vertical")>0 && (this.transform.rotation.eulerAngles.x >= 329 || transform.rotation.eulerAngles.x <= 25)) 
 		{
-			this.transform.Rotate(new Vector3(1,0,0), 5);
+			this.transform.RotateAround(new Vector3(0,0,0),new Vector3(1,0,0), 5);
+		}
+		else if (Input.GetAxis("Vertical")<0 && (this.transform.rotation.eulerAngles.x >= 335 || transform.rotation.eulerAngles.x <= 31)) 
+		{
+			this.transform.RotateAround(new Vector3(0,0,0),new Vector3(1,0,0), -5);
+		}
+		
+		if (Input.GetAxis("Horizontal")>0 && (this.transform.rotation.eulerAngles.z >= 335 || transform.rotation.eulerAngles.z <= 31)) 
+		{
+			this.transform.RotateAround(new Vector3(0,0,0),new Vector3(0,0,1), -5);
+		}
+		else if (Input.GetAxis("Horizontal")<0 && (this.transform.rotation.eulerAngles.z >= 329 || transform.rotation.eulerAngles.z <= 25)) 
+		{
+			this.transform.RotateAround(new Vector3(0,0,0),new Vector3(0,0,1), 5);
 		}
 	}
+	
+//	void Update () 
+//	{
+//		if (Input.GetAxis("Mouse Y")>0 && (this.transform.rotation.eulerAngles.x >= 329 || transform.rotation.eulerAngles.x <= 25)) 
+//		{
+//			this.transform.Rotate(new Vector3(1,0,0), 5);
+//		}
+//		else if (Input.GetAxis("Mouse Y")<0 && (this.transform.rotation.eulerAngles.x >= 335 || transform.rotation.eulerAngles.x <= 31)) 
+//		{
+//			this.transform.Rotate(new Vector3(1,0,0), -5);
+//		}
+//		
+//		if (Input.GetAxis("Mouse X")>0 && (this.transform.rotation.eulerAngles.z >= 329 || transform.rotation.eulerAngles.z <= 25)) 
+//		{
+//			this.transform.Rotate(new Vector3(0,0,1), 5);
+//		}
+//		else if (Input.GetAxis("Mouse X")<0 && (this.transform.rotation.eulerAngles.z >= 335 || transform.rotation.eulerAngles.z <= 31)) 
+//		{
+//			this.transform.Rotate(new Vector3(0,0,1), -5);
+//		}
+//	}
 }
