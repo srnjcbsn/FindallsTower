1b4fcd6281770d5cb5ecbe4c59a8f184f927f5fd
Søren Jacobsen <srnjcbsn@gmail.com>
2013-10-23 18:32:41 +0200

    Proper merge this time, I promise!

--- Findalls Tower/Assets/TiltPlane.cs
+++ Findalls Tower/Assets/TiltPlane.cs
@@ -5,15 +5,12 @@ public class TiltPlane : MonoBehaviour
 {
 	public float maxAngle;
 	
-	// Use this for initialization
 	void Start () 
 	{
 	
 	}
 	
-	// Update is called once per frame
 	void Update () 
-<<<<<<< HEAD
 	{	
 		Vector3 currentRotation = transform.localEulerAngles;
 		float minAngle = 360 - maxAngle;
@@ -44,49 +41,9 @@ public class TiltPlane : MonoBehaviour
 			
 			else
 				newZRotation = minAngle;
-=======
-	{
-		if (Input.GetAxis("Vertical")>0 && (this.transform.rotation.eulerAngles.x >= 329 || transform.rotation.eulerAngles.x <= 25)) 
-		{
-			this.transform.RotateAround(new Vector3(0,0,0),new Vector3(1,0,0), 5);
-		}
-		else if (Input.GetAxis("Vertical")<0 && (this.transform.rotation.eulerAngles.x >= 335 || transform.rotation.eulerAngles.x <= 31)) 
-		{
-			this.transform.RotateAround(new Vector3(0,0,0),new Vector3(1,0,0), -5);
-		}
-		
-		if (Input.GetAxis("Horizontal")>0 && (this.transform.rotation.eulerAngles.z >= 335 || transform.rotation.eulerAngles.z <= 31)) 
-		{
-			this.transform.RotateAround(new Vector3(0,0,0),new Vector3(0,0,1), -5);
-		}
-		else if (Input.GetAxis("Horizontal")<0 && (this.transform.rotation.eulerAngles.z >= 329 || transform.rotation.eulerAngles.z <= 25)) 
-		{
-			this.transform.RotateAround(new Vector3(0,0,0),new Vector3(0,0,1), 5);
->>>>>>> d7dcfa2d5e65c0cc3a3f8074e08b3397b87aca0f
 		}
 		
 		Vector3 newRotation = new Vector3 (newXRotation, 0, newZRotation);
 		transform.localEulerAngles = newRotation;
 	}
-	
-//	void Update () 
-//	{
-//		if (Input.GetAxis("Mouse Y")>0 && (this.transform.rotation.eulerAngles.x >= 329 || transform.rotation.eulerAngles.x <= 25)) 
-//		{
-//			this.transform.Rotate(new Vector3(1,0,0), 5);
-//		}
-//		else if (Input.GetAxis("Mouse Y")<0 && (this.transform.rotation.eulerAngles.x >= 335 || transform.rotation.eulerAngles.x <= 31)) 
-//		{
-//			this.transform.Rotate(new Vector3(1,0,0), -5);
-//		}
-//		
-//		if (Input.GetAxis("Mouse X")>0 && (this.transform.rotation.eulerAngles.z >= 329 || transform.rotation.eulerAngles.z <= 25)) 
-//		{
-//			this.transform.Rotate(new Vector3(0,0,1), 5);
-//		}
-//		else if (Input.GetAxis("Mouse X")<0 && (this.transform.rotation.eulerAngles.z >= 335 || transform.rotation.eulerAngles.z <= 31)) 
-//		{
-//			this.transform.Rotate(new Vector3(0,0,1), -5);
-//		}
-//	}
 }
