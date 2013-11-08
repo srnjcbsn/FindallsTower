ae835477a2d017c3719d05f1e0ca098015b8ece5
Nicolai Christensen <nicolai_christensen@hotmail.com>
2013-11-08 16:06:35 +0100

    Added a lot of prefabs in a new scene. Added scripts to some of the prefabs (like gravity, player movement, material change).

---
+++ Findalls Tower/Assets/Standard Assets/Prefabs/Items/ItemMat.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemMat : MonoBehaviour 
+{
+    public Material[] mats;
+
+    private int iter = 0;
+
+    MeshRenderer planeRenderer;
+
+    double time;
+    double timeOld;
+
+	// Use this for initialization
+	void Start () 
+    {
+        planeRenderer = GetComponentInChildren<MeshRenderer>();
+        planeRenderer.material = mats[iter];
+        timeOld = System.DateTime.Now.TimeOfDay.TotalMilliseconds;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        time = System.DateTime.Now.TimeOfDay.TotalMilliseconds;
+        if (time >= timeOld + 1000)
+        {
+            timeOld = time;
+            planeRenderer.material = mats[iter++];
+            if (iter == mats.Length)
+                iter = 0;
+        }
+        
+	}
+
+}
---
+++ Findalls Tower/Assets/Standard Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+
+    public float force;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidbody.AddForce(0f, 0f, force, ForceMode.Force);
+            
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigidbody.AddForce(0f, 0f, -force, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody.AddForce(force, 0f, 0f, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody.AddForce(-force, 0f, 0f, ForceMode.Force);
+        }
+	}
+}
