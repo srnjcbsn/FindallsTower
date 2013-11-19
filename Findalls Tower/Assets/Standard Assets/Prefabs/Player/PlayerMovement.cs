76fa9d303598f0c500dd46d91df6c264526e5f17
And <andreas.spejder@gmail.com>
2013-11-19 13:34:11 +0100

    PlayerLighting EnemyAI 2.0 Player Movement 2.0

--- Findalls Tower/Assets/Standard Assets/Prefabs/Player/PlayerMovement.cs
+++ Findalls Tower/Assets/Standard Assets/Prefabs/Player/PlayerMovement.cs
@@ -5,32 +5,53 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float force;
+    public int ReduceForceXFrame = 10;
+    public int moveSpeed = 2;
+    public int rotationSpeed = 3; //speed of turning
+    public float MaxVelocity = 1.7f;
 
-	// Use this for initialization
-	void Start () 
+    private Transform myTransform;
+    private int counter = 0;
+
+    void Awake()
     {
+        myTransform = transform; //cache transform data for easy access/preformance
+    }
+
+    void Start()
+    {
+        rigidbody.maxAngularVelocity = MaxVelocity;
+    }
 	
-	}
-	
-	// Update is called once per frame
 	void Update () 
     {
+        Vector3 infront = new Vector3(myTransform.position.x, myTransform.position.y, myTransform.position.z + 1);
+
+        myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+        Quaternion.LookRotation(infront - myTransform.position), rotationSpeed * Time.deltaTime);
+
         if (Input.GetKey(KeyCode.W))
         {
-            rigidbody.AddForce(0f, 0f, force, ForceMode.Force);
-            
+            //rigidbody.AddForce(0f, 0f, force, ForceMode.Force);
+            myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            rigidbody.AddForce(0f, 0f, -force, ForceMode.Force);
+            //rigidbody.AddForce(0f, 0f, -force, ForceMode.Force);
+            myTransform.position += -myTransform.forward * moveSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            rigidbody.AddForce(force, 0f, 0f, ForceMode.Force);
+            //rigidbody.AddForce(force, 0f, 0f, ForceMode.Force);
+            myTransform.position += myTransform.right * moveSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rigidbody.AddForce(-force, 0f, 0f, ForceMode.Force);
+            //rigidbody.AddForce(-force, 0f, 0f, ForceMode.Force);
+            myTransform.position += -myTransform.right * moveSpeed * Time.deltaTime;
         }
+
+        
+        //Debug.Log("" + rigidbody.angularVelocity);
 	}
 }
--- Findalls Tower/Assets/Standard Assets/Scripts/EnemyAI.cs
+++ Findalls Tower/Assets/Standard Assets/Scripts/EnemyAI.cs
@@ -4,90 +4,117 @@ using System.Collections;
 public class EnemyAI : MonoBehaviour {
 
     public float Force = 10;
-    public int ThinkEveryXFrame = 25;
-    private int counter = 0;
-    public double VisionRange = 0.4;
+    public int ThinkEveryXFrame = 2;    
+    public double VisionRange = 1.5;
+    public float moveSpeed = 2.2f;
+    public int rotationSpeed = 4; //speed of turning
+    public bool WanderPackage = true;
+    public bool FollowPackage = true;
 
-	// Use this for initialization
+    private int counter = 0;
+    private Transform targetTransform; 
+    private Transform myTransform;
+
+    void Awake()
+    {
+        myTransform = transform; //cache transform data for easy access/preformance
+    }
+
 	void Start () 
 	{
-	
+        targetTransform = GameObject.FindWithTag("Player").transform; //target the player	
 	}
 	
-	// Update is called once per frame
     void Update()
     {
+        GameObject target = GameObject.FindGameObjectsWithTag("Player")[0];
+
+        //Checks if player is within enemy vision range
+        if (System.Math.Abs(System.Math.Abs(target.transform.localPosition.x) - System.Math.Abs(this.transform.localPosition.x)) > VisionRange || 
+            System.Math.Abs(System.Math.Abs(target.transform.localPosition.z) - System.Math.Abs(this.transform.localPosition.z)) > VisionRange)
+            target = null;
+
+        
+        if (target != null)
+        {
+            //If player is within range and enemy has FollowPackage enabled, execute follow
+            if (FollowPackage)
+            {
+                Follow();
+                return;
+            }
+        }
+        
+        //Checks if the enemy is allowed to think
         counter++;
         if (counter % ThinkEveryXFrame != 0)
             return;
 
-        GameObject target= null;
-        target = GameObject.FindGameObjectsWithTag("Player")[0];
-
-        if (!((target.transform.localPosition.x > this.transform.localPosition.x - VisionRange && target.transform.localPosition.x < this.transform.localPosition.x + VisionRange)
-            //&& (target.transform.localPosition.y > this.transform.localPosition.y - VisionRange && target.transform.localPosition.y < this.transform.localPosition.y + VisionRange)
-            || (target.transform.localPosition.z > this.transform.localPosition.z - VisionRange && target.transform.localPosition.z < this.transform.localPosition.z + VisionRange)))
-            target = null;
-
-
-        if (target == null)
+        if (WanderPackage)
         {
-            Debug.Log("no target found -> wander");
             Wander();
+            return;            
         }
-        else
-        {
-            Debug.Log("target found -> chase");
-            Chase(target, counter);
-        }
-
+        //TODO: other behavior
     }
 
     void Wander()
 	{
         Debug.Log("Wander");
         System.Random rng = new System.Random();
+        Vector3 targetPosition;
 
-        int direction = rng.Next(0, 4);
-			
-		switch(direction)
-		{
-			case 0:
-			case 1:
-			case 2:
-			case 3:
-			default:
-                //rigidbody.AddForce(force, 0f, 0f, ForceMode.Force);
-			break;
-		}
+        //Pick random direction to walk towards
+        int directionX = rng.Next(-2, 3), directionZ = rng.Next(-2, 3);
+
+        targetPosition = new Vector3(myTransform.position.x + directionX, myTransform.position.y, myTransform.position.z + directionZ);
+
+        //Don't move if you are on top of the target
+        if (CheckIfTooCloseToTarget(targetPosition, 0.2))
+        {
+            Debug.Log("Skip Wander, target too close");
+            return;
+        }
+
+        //Face the player
+        myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+        Quaternion.LookRotation(targetPosition - myTransform.position), rotationSpeed * Time.deltaTime);
+
+        //move towards the player
+        myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
 	}
 
-    void Chase(GameObject target, int counter)
+    void Follow()
     {
-        Debug.Log("Chasing");
+        //proof-of-concept for LoS
+        var floor = GameObject.Find("FloorTest");
+        floor.layer = LayerMask.NameToLayer("FloorVis");        
 
-        bool XIsFurthestAway = false;
-        int direction = 1;
+        Vector3 targetPosition = new Vector3(targetTransform.position.x, myTransform.position.y, targetTransform.position.z);
 
-        if (System.Math.Abs(target.transform.localPosition.x - this.transform.localPosition.x) 
-            > System.Math.Abs(target.transform.localPosition.z - this.transform.localPosition.z))
-            XIsFurthestAway = true;
+        Debug.Log("Follow: "+targetPosition);
 
-        if (XIsFurthestAway)
+        //Don't move if you are on top of the target
+        if (CheckIfTooCloseToTarget(targetPosition, 0.2))
         {
-            if (this.transform.localPosition.x  > target.transform.localPosition.x)
-                direction = -1;
-        }
-        else
-        {
-            if (this.transform.localPosition.z  > target.transform.localPosition.z)
-                direction = -1;
+            Debug.Log("Can't Follow, target too close");
+            return;
         }
 
+        //rotate to look at the player
+        myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+        Quaternion.LookRotation(targetPosition - myTransform.position), rotationSpeed * Time.deltaTime);
 
-        if (XIsFurthestAway)
-            rigidbody.AddForce(direction*Force, 0f, 0f, ForceMode.Force);
-        else
-            rigidbody.AddForce(0f, 0f, direction*Force, ForceMode.Force);
+        //move towards the player
+        myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
     }
+
+    bool CheckIfTooCloseToTarget(Vector3 targetPosition, double precision)
+    {
+        if ((targetPosition.x + precision < myTransform.position.x && targetPosition.x - precision > myTransform.position.x) &&
+            (targetPosition.z + precision < myTransform.position.z && targetPosition.z - precision > myTransform.position.z))
+            return true;
+        else
+            return false;
+    }    
 }
