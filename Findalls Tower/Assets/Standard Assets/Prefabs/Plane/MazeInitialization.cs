64cf6d22af668c94f3cf616e151bd2854aff8fb5
Søren Jacobsen <srnjcbsn@gmail.com>
2013-12-05 21:31:06 +0100

    Included files forgotten in last commit

---
+++ Findalls Tower/Assets/Standard Assets/Prefabs/Plane/MazeInitialization.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using MazeGraph;
+
+public class MazeInitialization
+{
+	private PlaneScript planeScript;
+	
+	public Transform wallPrefab;
+	public Transform pathPrefab;
+	public Transform playerPrefab;
+	
+	public int mazeSize;
+	public int additionalPaths;
+	
+	private Point entryPosition;
+	private Point exitPosition;
+	
+	private Maze maze;
+	
+	public MazeInitialization 
+		( PlaneScript planeScript
+		, Transform wallPrefab 
+		, Transform pathPrefab 
+		, Transform playerPrefab 
+		, int mazeSize 
+		, int additionalPaths
+		)
+	{
+		this.planeScript = planeScript;
+		this.wallPrefab = wallPrefab;
+		this.pathPrefab = pathPrefab;
+		this.playerPrefab = playerPrefab;
+		this.mazeSize = mazeSize;
+		this.additionalPaths = additionalPaths;
+	}
+	
+	public Maze ConstructMaze ()
+	{	
+		maze = new Maze (mazeSize, additionalPaths);
+        
+		float scale = planeScript.scale;
+
+		foreach (Point pos in maze.AllPoints())
+		{	
+       		Transform tileTransform = null;
+			Tile tile = maze[pos];
+			
+			if (tile.GetType() == typeof(WallTile))
+			{
+				tileTransform = GenerateTile (wallPrefab, planeScript.MazeToPlaneCoords(pos), new Vector3 (scale, scale, scale));
+			}
+			
+			if (tile.GetType() == typeof(Tile))
+			{
+				tileTransform = GenerateTile (pathPrefab, planeScript.MazeToPlaneCoords(pos), new Vector3(scale, scale / 20f, scale));
+				
+				TileScript tscript = tileTransform.GetComponent<TileScript> ();
+				tscript.Model = tile;
+				tscript.planeScript = planeScript;
+			}
+			
+			if (tile.GetType() == typeof(EntryTile))
+				entryPosition = tile.Position;
+			
+			if (tile.GetType() == typeof(ExitTile))
+				exitPosition = tile.Position;
+			
+			planeScript.tileDict.Add(tile, tileTransform);
+			tileTransform.gameObject.layer = LayerMask.NameToLayer ("Hidden");
+		}		
+		
+		Transform tileTransform2;
+		// generate outer walls
+        tileTransform2 = GenerateTile(wallPrefab, new Vector3(0, scale / 2, -5 - (scale / 2)), new Vector3(10, scale, scale));   // South wall
+        tileTransform2.gameObject.layer = LayerMask.NameToLayer("Visible");
+        tileTransform2 = GenerateTile(wallPrefab, new Vector3(0, scale / 2, 5 + (scale / 2)), new Vector3(10, scale, scale));    // North wall
+        tileTransform2.gameObject.layer = LayerMask.NameToLayer("Visible");
+        tileTransform2 = GenerateTile(wallPrefab, new Vector3(5 + (scale / 2), scale / 2, 0), new Vector3(scale, scale, 10 + (scale * 2)));   // East Wall
+        tileTransform2.gameObject.layer = LayerMask.NameToLayer("Visible");
+        tileTransform2 = GenerateTile(wallPrefab, new Vector3(-5 - (scale / 2), scale / 2, 0), new Vector3(scale, scale, 10 + (scale * 2)));  // West Wall
+        tileTransform2.gameObject.layer = LayerMask.NameToLayer("Visible");
+		
+		return maze;
+	}
+	
+	private Transform GenerateTile (Transform TileType, Vector3 position, Vector3 scaleVector)
+	{
+		Transform tile = (Transform) MonoBehaviour.Instantiate (TileType);
+		tile.parent = planeScript.transform;
+		tile.localPosition = position;
+		tile.localScale = scaleVector;
+		
+		return tile;
+	}
+	
+	public void PopulateMaze ()
+	{
+		Transform player = (Transform) MonoBehaviour.Instantiate (playerPrefab);
+		player.parent = planeScript.transform;
+		player.position = planeScript.MazeToPlaneCoords (entryPosition);
+		player.localScale = new Vector3 (.5f,.5f,.5f);
+	}
+}
---
+++ Findalls Tower/Assets/Standard Assets/Prefabs/Plane/PlaneScript.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using MazeGraph;
+using System.Linq;
+
+public class PlaneScript : MonoBehaviour 
+{
+	public Transform WallPrefab;
+	public Transform PathPrefab;
+	public Transform PlayerPrefab;
+	public int mazeSize;
+	public int additionalPaths;
+	
+	private Maze maze;
+	internal float scale;
+	private Dictionary<GameObject, HashSet<Tile>> tilesRevealedBy;
+	public Dictionary<Tile, Transform> tileDict;
+	
+	public Maze Maze {
+		get { return maze; } 
+	}
+	
+	void Start () 
+	{
+		scale = 10f / mazeSize;
+		tileDict = new Dictionary<Tile, Transform> ();
+		tilesRevealedBy = new Dictionary<GameObject, HashSet<Tile>> ();
+		MazeInitialization mazeInitializer = new MazeInitialization 
+			(
+				this, 
+				WallPrefab, 
+				PathPrefab,
+				PlayerPrefab,
+				mazeSize, 
+				additionalPaths
+			);
+		maze = mazeInitializer.ConstructMaze ();
+		mazeInitializer.PopulateMaze ();
+	}
+	
+	public Vector3 MazeToPlaneCoords (Point mazeCoords)
+	{
+		int offset = 5;
+		
+		float scalefactor = (float)mazeSize / 10f;
+		
+		float planeX = (mazeCoords.X / scalefactor) - offset + scale / 2;
+		float planeY = (mazeCoords.Y / scalefactor) - offset + scale / 2;
+		
+		return new Vector3(planeX, scale / 2, planeY);
+	}
+	
+	public Point PlaneToMazeCoords (Vector3 planeCoords)
+	{
+		int offset = 5;
+		float scalefactor = (float)mazeSize / 10f;
+		
+		int mazeX = Mathf.FloorToInt( (planeCoords.x + offset) * scalefactor);
+		int mazeY = Mathf.FloorToInt( (planeCoords.z + offset) * scalefactor);
+		
+		return new Point(mazeX, mazeY);
+	}
+	
+	public Tile GetTileOfPoint(Point pos)
+    {
+        return maze[pos];
+    }
+	
+	public bool AreTilesWithinRange(Tile t1, Tile t2, int range)
+	{
+		return maze.AreTilesWithinRange(t1, t2, range);
+	}
+	
+	public void RevealTilesInStraightPath (GameObject revealer, Tile fromTile, int range)
+	{
+		HashSet<Tile> revealed = new HashSet<Tile> ();
+		
+		foreach (Tile tile in TilesInPathWithWalls (fromTile, range))
+		{
+			tileDict[tile].GetComponent<TileVisibility> ().Reveal (revealer);
+			revealed.Add (tile);
+		}
+		
+		tilesRevealedBy.Add (revealer, revealed);
+	}
+	
+	public void HideTilesRevealedBy (GameObject hider)
+	{
+		if (!tilesRevealedBy.ContainsKey (hider))
+			return;
+		
+		foreach (Tile tile in tilesRevealedBy[hider])
+			tileDict[tile].GetComponent<TileVisibility> ().Hide (hider);
+		
+		tilesRevealedBy.Remove(hider);
+	}
+	
+	private IEnumerable<Tile> TilesInPathWithWalls (Tile fromTile, int range)
+	{
+		HashSet<Vertex> vertices = new HashSet<Vertex> ();
+		
+		vertices.UnionWith (VerticesInPath (fromTile, new Point (1, 0), range));
+		vertices.UnionWith (VerticesInPath (fromTile, new Point (0, 1), range));
+		vertices.UnionWith (VerticesInPath (fromTile, new Point (-1, 0), range));
+		vertices.UnionWith (VerticesInPath (fromTile, new Point (0, -1), range));
+		
+		foreach (Vertex vertex in vertices)
+		{
+			yield return vertex.Content;
+			foreach (Vertex adjWall in vertex.Adjacent.Where (vert => vert.Content.GetType () == typeof(WallTile)))
+				yield return adjWall.Content;
+		}
+	}
+		
+	private IEnumerable<Vertex> VerticesInPath (Tile fromTile, Point direction, int range)
+	{
+		
+		for (int i = 0; i < range; i++)
+		{
+			Point newPoint = fromTile.Position + new Point (direction.X * i, direction.Y * i);
+			
+			int max = maze.Dimension;
+			if (newPoint.X >= max || newPoint.X < 0 || newPoint.Y >= max || newPoint.Y < 0)
+				break; // handle this in maze[Point] property
+			
+			Vertex newVertex = maze.MazeGraph [newPoint];
+			
+			if (newVertex.Content.GetType() == typeof (WallTile))
+				break;
+			
+			yield return newVertex;
+		}
+	}
+	
+	private bool TileInRange (Point fromPos, Point pos, int range)
+	{	
+		return (pos.X == fromPos.X && Mathf.Abs(Mathf.Abs(fromPos.Y) - Mathf.Abs(pos.Y)) < range)
+				|| (pos.Y == fromPos.Y && Mathf.Abs(Mathf.Abs(fromPos.X) - Mathf.Abs(pos.X)) < range);
+	}
+}
