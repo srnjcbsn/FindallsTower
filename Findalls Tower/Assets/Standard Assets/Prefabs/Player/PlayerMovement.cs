485f35d361d459791b6acdbef073aabacb26aff3
Søren Jacobsen <srnjcbsn@gmail.com>
2013-12-17 13:42:13 +0100

    Player is now moved by applying forces

--- Findalls Tower/Assets/Standard Assets/Prefabs/Player/PlayerMovement.cs
+++ Findalls Tower/Assets/Standard Assets/Prefabs/Player/PlayerMovement.cs
@@ -1,60 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour 
+public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 1.1f;
-    public int rotationSpeed = 10; //speed of turning
-    public float MaxVelocity = 1.7f;
+	public float moveSpeed;
+	public float MaxVelocity;
 
-    private Transform myTransform;
-    private int counter = 0;
+	void Start ()
+	{
+		rigidbody.maxAngularVelocity = MaxVelocity;
+	}
 
-    void Awake()
-    {
-        myTransform = transform; //cache transform data for easy access/preformance
-    }
+	void Update ()
+	{
+		Vector3 direction = Vector3.zero;
 
-    void Start()
-    {
-        rigidbody.maxAngularVelocity = MaxVelocity;
-    }
-	
-	void Update () 
-    {
-        //Vector3 infront = new Vector3(myTransform.position.x, myTransform.position.y, myTransform.position.z + 10);
+		if (Input.GetKey (KeyCode.W))
+			direction += Vector3.forward;
+		if (Input.GetKey (KeyCode.S))
+			direction -= Vector3.forward;
+		if (Input.GetKey (KeyCode.D))
+			direction += Vector3.right;
+		if (Input.GetKey (KeyCode.A))
+			direction -= Vector3.right;
 
-        //myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
-        //Quaternion.LookRotation(infront - myTransform.position), rotationSpeed * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            //rigidbody.AddForce(0f, 0f, force, ForceMode.Force);
-            myTransform.localPosition += Vector3.forward * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            //rigidbody.AddForce(0f, 0f, -force, ForceMode.Force);
-            myTransform.localPosition += -Vector3.forward * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            //rigidbody.AddForce(force, 0f, 0f, ForceMode.Force);
-            myTransform.localPosition += Vector3.right * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            //rigidbody.AddForce(-force, 0f, 0f, ForceMode.Force);
-            myTransform.localPosition += -Vector3.right * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            rigidbody.isKinematic = !rigidbody.isKinematic;
-        }
+		rigidbody.AddForce (direction * moveSpeed, ForceMode.VelocityChange);
 
 		if (rigidbody.velocity.magnitude > MaxVelocity)
-        	rigidbody.velocity = rigidbody.velocity.normalized * MaxVelocity;
-		
-        //Debug.Log("vel: " + myTransform.rigidbody.velocity);
+			rigidbody.velocity = rigidbody.velocity.normalized * MaxVelocity;
 	}
 }
